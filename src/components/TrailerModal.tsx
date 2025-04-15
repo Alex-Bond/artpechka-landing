36732f08e9f466731afbc65e0ee522ef17fe5044
Alex Bondarevskyi <me@alexbond.info>
2025-04-15 16:55:02 -0500

    Changing video player

--- src/components/TrailerModal.tsx
+++ src/components/TrailerModal.tsx
@@ -1,8 +1,7 @@
-
-import React from 'react';
-import { Dialog, DialogContent, DialogTitle, DialogClose } from '@/components/ui/dialog';
-import { X } from 'lucide-react';
+import React from 'react'
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button.tsx'
+import ReactPlayer from 'react-player'
 
 interface TrailerModalProps {
   isOpen: boolean;
@@ -14,21 +13,13 @@ interface TrailerModalProps {
 const TrailerModal = ({ isOpen, onClose, title, videoUrl }: TrailerModalProps) => {
   if (!videoUrl) return null;
 
-  // Extract video ID from Vimeo URL
-  const videoId = videoUrl.split('/').pop();
-
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
       <DialogContent className="max-w-[98vw] w-full h-[98vh] p-0 overflow-hidden bg-transparent border-0">
         <DialogTitle className="sr-only">{title}</DialogTitle>
         <div className="flex flex-col h-full">
           <div className="w-full h-[95vh] flex flex-col">
-            <iframe
-              src={`https://player.vimeo.com/video/${videoId}?h=00000000&autoplay=0&title=0&byline=0&portrait=0`}
-              allow="autoplay; fullscreen; picture-in-picture"
-              className="w-full h-full"
-              title={title}
-            />
+            <ReactPlayer controls playing url={videoUrl} width='100%' height='100%' />
 
             <div className='mt-10 w-full text-center'>
               <Button variant='default' onClick={()=> onClose()}>Close Video</Button>
